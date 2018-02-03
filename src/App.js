07b9d8b1775c16d7d7af4e933b3f02ddccbe1e7c
Daniel Pop <vdanielpop@gmail.com>
2018-02-03 16:43:07 +0200

    Added mocks & separated Aggregate components.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,67 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'Daniel',
+    playlists: [
+      {
+        name: 'My favorites',
+        songs: [
+          {name: 'Song 1', duration: 186},
+          {name: 'Song 2', duration: 229},
+          {name: 'Song 3', duration: 227}
+        ],
+      },
+      {
+        name: 'Disco Playlist',
+        songs: [
+          {name: 'Disco Song 1', duration: 201},
+          {name: 'Disco Song 2', duration: 239},
+          {name: 'Disco Song 3', duration: 231}
+        ],
+      },
+      {
+        name: 'Dubstep Playlist',
+        songs: [
+          {name: 'Dubstep Song 1', duration: 217},
+          {name: 'Dubstep Song 2', duration: 217},
+          {name: 'Dubstep Song 3', duration: 228}
+        ],
+      },
+      {
+        name: 'Acoustic Playlist',
+        songs: [
+          {name: 'Acoustic Song 1', duration: 223},
+          {name: 'Acoustic Song 2', duration: 213},
+          {name: 'Acoustic Song 3', duration: 219}
+        ],
+      },
+    ],
+  }
+}
+
+class PlaylistCounter extends Component {
   render () {
     return (
       <div style={{width: '40%', display: 'inline-block'}}>
-        <h2>Text: Number</h2>
+        <h2>{this.props.playlists.length}</h2>
+      </div>
+    )
+  }
+}
+
+class HoursCounter extends Component {
+  render () {
+    let allSongs = this.props.playlists.reduce((songs, playlist) => {
+      return songs.concat(playlist.songs)
+    }, [])
+    let totalDuration = allSongs.reduce((total, song) => {
+      return total + song.duration
+    }, 0)
+    return (
+      <div style={{width: '40%', display: 'inline-block'}}>
+        <h2>{totalDuration} seconds</h2>
       </div>
     )
   }
@@ -40,20 +96,33 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {serverData: {}}
+  }
+
+  componentDidMount () {
+    setTimeout(() => {this.setState({serverData: fakeServerData})}, 1500)
+  }
+
   render () {
     let name = 'Daniel'
     return (
       <div className="App">
-        <h1>Title of the page</h1>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <div>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-        </div>
+        {
+          this.state.serverData.user ? <div>
+            <h1>{this.state.serverData.user && this.state.serverData.user.name}'s playlist</h1>
+            < PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+            <HoursCounter playlists={this.state.serverData.user.playlists}/>
+            <Filter/>
+            <div>
+              <Playlist/>
+              <Playlist/>
+              <Playlist/>
+              <Playlist/>
+            </div>
+          </div> : <h1>Loading...</h1>
+        }
       </div>
     )
   }
