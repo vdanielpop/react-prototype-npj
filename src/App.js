6fcf6961df61f69fb09c5cee287d75bc5a639f3d
Daniel Pop <vdanielpop@gmail.com>
2018-02-09 21:57:25 +0200

    Filter applies to the header Counters.

--- src/App.js
+++ src/App.js
@@ -112,19 +112,20 @@ class App extends Component {
 
   render () {
     let name = 'Daniel'
+    let filteredPlaylist = this.state.serverData.user ? this.state.serverData.user.playlists.filter(playlist =>
+      playlist.name.toLowerCase().includes(
+        this.state.filterString.toLowerCase())
+    ) : []
     return (
       <div className="App">
         {
           this.state.serverData.user ? <div>
             <h1>{this.state.serverData.user && this.state.serverData.user.name}'s playlist</h1>
-            < PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-            <HoursCounter playlists={this.state.serverData.user.playlists}/>
+            < PlaylistCounter playlists={filteredPlaylist}/>
+            <HoursCounter playlists={filteredPlaylist}/>
             <Filter onTextChange={text => this.setState({filterString: text})}/>
             <div>
-              {this.state.serverData.user.playlists.filter(playlist =>
-                playlist.name.toLowerCase().includes(
-                  this.state.filterString.toLowerCase())
-              ).map(playlist =>
+              {filteredPlaylist.map(playlist =>
                 <Playlist playlist={playlist}/>
               )}
             </div>
